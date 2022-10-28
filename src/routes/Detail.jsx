14735a9de3dc4387886575a2dc2caf54dbbb1673
Vbeo147 <vbeo147@gmail.com>
2022-10-28 16:26:19 +0900

    Detail jsx

--- src/routes/Detail.jsx
+++ src/routes/Detail.jsx
@@ -1,5 +1,33 @@
-function Detail() {
-  return <div>Detail</div>;
+import React from "react";
+import { connect } from "react-redux";
+import { useParams, useNavigate } from "react-router-dom";
+import { actionCreators } from "../store";
+
+function Detail({ toDos, onBtnClick }) {
+  const { id } = useParams();
+  const toDo = toDos.find((toDo) => toDo.id === parseInt(id));
+  const navigate = useNavigate();
+  const handleDelete = () => {
+    onBtnClick(parseInt(id));
+    navigate(-1);
+  };
+  return (
+    <>
+      <h1>{toDo?.text}</h1>
+      <h5>Create at : {toDo?.id}</h5>
+      <div>
+        <button onClick={handleDelete}>DEL</button>
+      </div>
+    </>
+  );
 }
 
-export default Detail;
+function mapStateToProps(state) {
+  return { toDos: state };
+}
+
+function mapDispatchToProps(dispatch) {
+  return { onBtnClick: (id) => dispatch(actionCreators.deleteToDo(id)) };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Detail);
